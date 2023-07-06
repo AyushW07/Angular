95f69bf2ab0d0910734568ee4a9fe88f2f8d94d1
aayush <ayushwasnik999@gmail.com>
2023-07-06 16:10:02 +0530

    added Todo Add functionality

--- src/app/Components/todo-item/todo-item.component.ts
+++ src/app/Components/todo-item/todo-item.component.ts
@@ -8,11 +8,17 @@ import { Todo } from 'src/app/Todo';
 })
 export class TodoItemComponent {
   @Input() todo!: Todo;
+  @Input() i!: number;
   @Output() todoDelete: EventEmitter<Todo> = new EventEmitter();
+  @Output() todoCheckbox: EventEmitter<Todo> = new EventEmitter();
 
   onClick(todo: Todo) {
     this.todoDelete.emit(todo);
     console.log('onClick is trigerred');
+  }
+
+  onCheckCLick(todo: Todo) {
+    this.todoCheckbox.emit(todo);
   }
 
   constructor() {}
